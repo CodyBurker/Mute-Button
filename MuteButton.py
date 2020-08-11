f3ea25308f7f22262f0a0af6f7d6787490f24fe2
Cody Burker <cody.burker@franklincollege.edu>
2020-08-10 23:14:52 -0400

    Reorganized Code

--- MuteButton.py
+++ MuteButton.py
@@ -7,9 +7,7 @@ import serial
 import getZoomStatus
 import threading
 
-keyboard = keyboard_controller()
-
-
+# Function to toggle mute via keyboard emulation
 def toggle_mute():
     # Bring Zoom to focus
     # Press shift, alt, control:
@@ -23,9 +21,6 @@ def toggle_mute():
         keyboard.press("a")
         keyboard.release("a")
 
-
-ser = serial.Serial(port="COM4")
-
 # Helper function to print from within subprocess
 def pt(status):
     print(status)
@@ -38,26 +33,52 @@ def get_button(ser):
         toggle_mute()
         print(x)
 
-
+# Function to send code to toggle LED from Arduino
 def light_on(light_on_var, ser):
-    if light_on_var == True:
-        ser.write(b"off")
-    else:
-        ser.write(b"on")
+    try:
+        if light_on_var == True:
+            ser.write(b"off")
+        else:
+            ser.write(b"on")
+    except: 
+        pt("Error")
+
+# Variable for keyboard controller
+keyboard = keyboard_controller()
 
 
-thread = threading.Thread(target=get_button, args=(ser,))
-am_muted = False
-
-thread.start()
-
-
+# Variable to keep track of mute status
+am_muted = True
+# Loop to keep running in background:
 while True:
-    if getZoomStatus.DetectZoomMeeting():
-        pt("Meeting detected")
-        am_muted_latest = "zoomMute:muted" in getZoomStatus.GetZoomStatus()
-        # pt("Latest:" +  am_muted_latest)
-        if am_muted != am_muted_latest:
-            # pt("Changed:" + am_muted_latest)
-            light_on(not am_muted, ser)
-        am_muted = am_muted_latest
+    try:
+        print("Trying Serial Connection:")
+        # Set up serial connection
+        ser = serial.Serial(port="COM4")
+        print("Successful Serial Connection")
+        # Set up multithreading
+        thread = threading.Thread(target=get_button, args=(ser,))
+        # Start background thread to keep track of button presses
+        thread.start()
+        # Loop to sync light status
+        while True:
+            # Check whether a meeting is open on the computer:
+            try: 
+                if getZoomStatus.DetectZoomMeeting():
+                    # If there is a meeting, check whether I am muted or not:
+                    print("\tMeeting detected")
+                    try:
+                        am_muted_latest = "zoomMute:muted" in getZoomStatus.GetZoomStatus()
+                        # pt("Latest:" +  am_muted_latest)
+                        if am_muted != am_muted_latest:
+                            print("\t\tGot mute status")
+                            # pt("Changed:" + am_muted_latest)
+                            light_on(not am_muted, ser)
+                        am_muted = am_muted_latest
+                    except:
+                        print("\t\tError getting mute stats")
+            except:
+                print("\tError getting Zoom meeting")
+    except: 
+        print("No device. Attempting reconnect in 10 seconds.")
+        time.sleep(10)
